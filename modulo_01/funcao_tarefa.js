4bf544ee2e90968ff48a92a5a11ff0a29e637736
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2025-08-15 15:31:32 -0300

    Fix: coloca for dentro de uma função

--- modulo_01/funcao_tarefa.js
+++ modulo_01/funcao_tarefa.js
@@ -1,7 +1,11 @@
 //Exercício de exemplo: mostrar apenas números pares de um a dez
 
-for (let i = 1; i <=10; i++) {
-  if (i % 2 === 0) {
-    console.log(i + " é par");
-  }
+function exercicio() {
+  for (let i = 1; i <=10; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " é par");
+    }
+  };
 };
+
+exercicio();
