f506a60aafdcc9e71aa37c5b9afc0b39f094d618
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2025-08-24 01:48:54 -0300

    Feat: adiciona função que calcula o fatorial de um número

--- modulo_01/atividades_javascript/atividade_javascript_01/atividade_funcoes.js
+++ modulo_01/atividades_javascript/atividade_javascript_01/atividade_funcoes.js
@@ -9,3 +9,24 @@ function verificaParImpar(numero) {
 
 console.log(verificaParImpar(10));
 console.log(verificaParImpar(3));
+
+// Função que calcula o fatorial de um número
+function calculaFatorial(numero) {
+  if (numero < 0) {
+    return "Fatorial não é definido para números negativos, escolha outro número.";
+  } else if (numero === 0) {
+    return `o fatorial de ${numero} é 1.`;
+  } else {
+    let fatorial = 1;
+    
+    for (let i = 1; i <= numero; i++) {
+      fatorial *= i;
+    }
+    
+    return `o fatorial de ${numero} é ${fatorial}.`;
+  }
+};
+
+console.log(calculaFatorial(-3));
+console.log(calculaFatorial(0));
+console.log(calculaFatorial(5));
